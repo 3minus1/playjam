5728828d023ce807139f342ecef1d1650f15cc05
Akshat <akforsn@gmail.com>
2017-04-04 10:07:46 +0530

    Navbar wrapped in a container

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -7,11 +7,13 @@
 
 <nav>
     <div class="nav-wrapper">
-      <a href="/" class="brand-logo">PlayJam</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        @if(Auth::user())
-        	<li><a class="dropdown-button" data-activates="userDropdown" href="#">{{Auth::user()->name}}<i class="material-icons right">arrow_drop_down</i></a></li>
-        @endif
-      </ul>
+      <div class="container">
+          <a href="/" class="brand-logo">PlayJam</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            @if(Auth::user())
+            	<li><a class="dropdown-button" data-activates="userDropdown" href="#">{{Auth::user()->name}}<i class="material-icons right">arrow_drop_down</i></a></li>
+            @endif
+          </ul>
+      </div>
     </div>
 </nav>
